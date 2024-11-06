f6382f067387e73675fc205f3b3fba1049ada9bd
MaksymB <m.bychyniuk@gmail.com>
2024-11-06 07:17:31 +0200

    add app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NewsComponent } from './news/news.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AuthGuard } from './auth.guard';
+import {AppComponent} from './app.component';
+import {FormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+];
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    FormsModule
+  ],
+  exports: [RouterModule],
+  providers: [],
+})
+export class AppModule { }
